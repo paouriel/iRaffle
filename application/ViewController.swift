29e538b7e03cfb003e2cb062c050867e812f7a8c
ITS-OJT <its-ojt@gleens-mac-mini.local>
2018-01-18 14:58:20 +0800

    Initial Commit

---
+++ application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  application
+//
+//  Created by ITS-OJT on 18/01/2018.
+//  Copyright © 2018 ITS-OJT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
